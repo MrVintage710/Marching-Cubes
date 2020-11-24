a12413d6d2a8a00341197ade2ed5952d8a151299
Brooks Palin <brookspalin710@gmail.com>
2020-11-24 16:51:40 -0700

    asd

---
+++ Assets/Scripts/nodeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nodeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Renderer>().material.color = Color.white;
+        this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.05f);
+    }
+
+    void OnMouseDown()
+    {
+        if(this.GetComponent<Renderer>().material.color == Color.white)
+        {
+            this.GetComponent<Renderer>().material.color = Color.grey;
+            // add to the mesh
+        }
+        else
+        {
+            this.GetComponent<Renderer>().material.color = Color.white;
+            //remove from mesh
+        }
+    }
+
+    void OnMouseOver()
+    {
+        this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    }
+    void OnMouseExit()
+    {
+        this.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 0.05f);
+    }
+}
+
---
+++ Assets/Scripts/startupCode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startupCode : MonoBehaviour
+{
+    int chunkNumber = 1;
+    public GameObject Node;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int nodeScript = chunkNumber * 25;
+
+        for(int i = 0; i<= nodeScript; i++)
+        {
+            for(int r = 0; r <= nodeScript; r++)
+            {
+                for(int t = 0; t<= nodeScript; t++)
+                {
+                    Instantiate(Node, new Vector3(i, r, t), Quaternion.identity);
+                }
+            }
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
